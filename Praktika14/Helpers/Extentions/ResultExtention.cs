2ae59826d6c5a5b7f4a8e59edd505351edece13a
roya06 <royamasimli@gmail.com>
2025-10-29 12:44:25 +0400

    praktikauni

---
+++ Praktika14/Helpers/Extentions/ResultExtention.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika14.Helpers.Extentions
+{
+    
+    public static class ResultExtention
+    {
+        #region //int array ucun extention method yazin .Hemin method arrayin elementlerinin hasilini qaytarsin
+        public static int MultiplyElements(this int[] numbers)
+        {
+            int result = 1;
+            foreach (var item in numbers)
+            {
+                result *= item;
+            }
+            return result;
+
+        }
+        #endregion
+        #region //Console-dan email gelecek .Emailde @ isharesinin bir eded olub olmamasini yyoxlayan ve bool qayataran extention method yazin.
+        
+        public static bool SignVerification(this string email)
+        {
+            int count = 0;
+
+            foreach (char c in email)
+            {
+                if (c == '@')
+                    count++;
+            }
+
+            return count == 1;
+        }
+        #endregion
+    }
+}
---
+++ Praktika14/Program.cs
@@ -0,0 +1,28 @@
+﻿using Praktika14.Helpers.Extentions;
+
+namespace Praktika14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region //int array ucun extention method yazin .Hemin method arrayin elementlerinin hasilini qaytarsin
+            int[] nums = { 2, 3, 4 };
+            int result = nums.MultiplyElements();
+
+            Console.WriteLine($"Array elementlerinin hasili: {result}");
+            #endregion
+            #region //Console-dan email gelecek .Emailde @ isharesinin bir eded olub olmamasini yyoxlayan ve bool qayataran extention method yazin.
+            Console.Write("Email daxil edin: ");
+            string email = Console.ReadLine();
+
+            bool result1 = email.SignVerification();
+
+            if (result1) 
+                Console.WriteLine("Email duzgundur (1 eded @ var).");
+            else
+                Console.WriteLine("Email sehvdir (@ isaresi yoxdur ya da birden coxdur).");
+            #endregion
+        }
+    }
+}
